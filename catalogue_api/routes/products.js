0c5715d5ecd047feaf3be239cc9118df28d0a7be
Jia Yu Koh <kohjiayu1998@gmail.com>
2024-01-23 03:32:35 +0000

    added app.get for products

--- catalogue_api/routes/products.js
+++ catalogue_api/routes/products.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const { getDB } = require('../mongoUtil');
+const { authenticateToken } = require('../middlewares');
 const { ObjectId } = require('mongodb');
 require('dotenv').config();
+const app = express()
 const router = express.Router();
 const COLLECTION = 'products';
 
 
-app.post("/", authenticateToken, async function (req, res) {
+app.post("/products", authenticateToken, async function (req, res) {
     // anything retrieved is from req.body is a string, not number
     try {
         const { name, uom, category, price } = req.body;
@@ -24,6 +26,65 @@ app.post("/", authenticateToken, async function (req, res) {
       }
 
 });
+
+app.get('/products', async (req, res) => {
+    try {
+        const products = await db.collection('products').find({}).toArray();
+        res.json(products);
+
+        const uoms = await db.collection('uom').find({}).toArray();
+        const uomMap = {};
+
+        for (let i = 0; i < uoms.length; i++) {
+            const uom = uoms[i];
+            uomMap[uom._id] = uom;
+        }
+
+        for (let j = 0; j < products.length; j++) {
+            const product = products[j];
+            if (product.uom && uomMap[product.uom._id]) {
+                product.uom = uomMap[product.uom._id];
+            }
+        }
+
+        const categories = await db.collection('category').find({}).toArray();
+        const categoryMap = {};
+
+        for (let i = 0; i< categories.length; i++) {
+            const category = categories[i];
+            categoryMap[category._id] = category;
+        }
+
+        for (let j = 0; j < products.length; j++) {
+            const products = products[j];
+            if(Array.isArray(products.categories)) {
+                products.categories = products.categories.map(categoryId => {
+                    return categoryMap[categoryId] || categoryId;
+                })
+            }
+        }
+
+    res.json(products);
+
+    }catch (error) {
+        res.status(500).json({message:'Error fetching products', error:error.message})
+    }
+});
+
+app.get('/products/:id', async (req, res) => {
+    try {
+      const id = new ObjectId(req.params.id);
+      const product = await db.collection('products').findOne({_id: id});
+      if (product) {
+        res.json(product);
+      } else {
+        res.status(404).json({ message: 'Product not found' });
+      }
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching product', error: error.message });
+    }
+  });
+
 
 
 app.delete("/food/:foodRecordId", async function (req, res) {
