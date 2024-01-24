3ca5515af4851dccf52041c0ef610f25b377317c
Jia Yu Koh <kohjiayu1998@gmail.com>
2024-01-24 11:57:49 +0000

    created category.js and uom.js

---
+++ catalogue_api/routes/category.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const { getDB } = require('../mongoUtil');
+const { authenticateToken } = require('../middlewares');
+const { ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express()
+const router = express.Router();
+const COLLECTION = 'uom';
+
+
+app.post("/category", authenticateToken, async function (req, res) {
+    // anything retrieved is from req.body is a string, not number
+    try {
+        const { name } = req.body;
+    
+        // Validation
+        if (!name) {
+          return res.status(400).json({ message: 'Missing required fields' });
+        }
+    
+        const newCategory = { name };
+        const result = await db.collection('category').insertOne(newCategory);
+        res.status(201).json(result);
+      } catch (error) {
+        res.status(500).json({ message: 'Error adding new product', error: error.message });
+      }
+
+});
+
+app.get('/category', async (req, res) => {
+    try {
+        const categories = await db.collection('category').find({}).toArray();
+        res.json(categories);
+    } catch (error) {
+        res.status(500).json({message: 'Error fetching category', error: error.message});
+    }
+});
+
+app.delete("/category:id", async function (req, res) {
+    try {
+        const categoryId = req.params.id;
+        const result = await db.collection('category').deleteOne({_id: categoryId});
+        if (result.deleteCount === 0 ) {
+            return res.status(404).json({ message:"category not found"})
+        }
+
+        res.status(200).json({ message: "category successfully deleted"})
+    } catch (error) {
+        res.status(500).json({message: 'Error deleting category', error: error.message})
+    }
+});
+
+
+app.put("/category/:id", async function (req, res) { 
+    try {
+     const categoryId = req.params.id;
+     const { name } = req.body
+ 
+     if (!name) {
+         return res.status(400).json({ message: 'Missing required fields' });
+     }
+ 
+     const objectId = new ObjectId(categoryId); 
+ 
+     const updateData = { name }; 
+ 
+     const result = await db.collection('name').updateOne(
+         { _id: objectId }, 
+         { $set: updateData } 
+     );
+ 
+     if (result.matchedCount === 0) {
+         res.status(404).json({ message: 'Category not found' });
+     } else if (result.modifiedCount === 0) {
+         res.status(200).json({ message: 'No changes made to the Category' });
+     } else {
+         res.status(200).json({ message: 'Category updated successfully' });
+     }
+    } catch (error) {
+         res.status(500).json({ message: 'Error updating UoM', error: error.message });
+    }
+ });
+
+
+module.exports = router
--- catalogue_api/routes/uom.js
+++ catalogue_api/routes/uom.js
@@ -29,81 +29,40 @@ app.post("/uom", authenticateToken, async function (req, res) {
 
 app.get('/uom', async (req, res) => {
     try {
-        const products = await db.collection('products').find({}).toArray();
-
-        // Fetch and map UOMs
-        const uomIds = products.map(product => product.uom && product.uom._id).filter(id => id);
-        const uoms = await db.collection('uom').find({_id: {$in: uomIds}}).toArray();
-        const uomMap = {};
-        uoms.forEach(uom => uomMap[uom._id] = uom);
-
-        // Fetch and map Categories
-        const categoryIds = products.reduce((acc, product) => acc.concat(product.category || []), []);
-        const categories = await db.collection('category').find({_id: {$in: categoryIds}}).toArray();
-        const categoryMap = {};
-        categories.forEach(category => categoryMap[category._id] = category);
-
-        // Enrich products with UOM and Category
-        products.forEach(product => {
-            if (product.uom && uomMap[product.uom._id]) {
-                product.uom = uomMap[product.uom._id];
-            }
-            if (product.category) {
-                product.category = product.category.map(categoryId => categoryMap[categoryId] || categoryId);
-            }
-        });
-
-        res.json(products);
-
+        const uoms = await db.collection('uom').find({}).toArray();
+        res.json(uoms);
     } catch (error) {
-        res.status(500).json({message: 'Error fetching products', error: error.message});
+        res.status(500).json({message: 'Error fetching uom', error: error.message});
     }
 });
 
-
-/*app.get('/products/:id', async (req, res) => {
+app.delete("/uom:id", async function (req, res) {
     try {
-      const id = new ObjectId(req.params.id);
-      const product = await db.collection('products').findOne({_id: id});
-      if (product) {
-        res.json(product);
-      } else {
-        res.status(404).json({ message: 'Product not found' });
-      }
-    } catch (error) {
-      res.status(500).json({ message: 'Error fetching product', error: error.message });
-    }
-  });*/
-
-
-
-app.delete("/products:id", async function (req, res) {
-    try {
-        const productId = req.params.id;
-        const result = await db.collection('products').deleteOne({_id: productId});
+        const uomId = req.params.id;
+        const result = await db.collection('uom').deleteOne({_id: uomId});
         if (result.deleteCount === 0 ) {
-            return res.status(404).json({ message:"Product not found"})
+            return res.status(404).json({ message:"UoM not found"})
         }
 
-        res.status(200).json({ message: "Product successfully deleted"})
+        res.status(200).json({ message: "UoM successfully deleted"})
     } catch (error) {
-        res.status(500).json({message: 'Error deleting product', error: error.message})
+        res.status(500).json({message: 'Error deleting UoM', error: error.message})
     }
 });
 
 
-app.put("/products/:id", async function (req, res) { 
+app.put("/uom/:id", async function (req, res) { 
     try {
-     const productId = req.params.id;
-     const { name, uom, category, price } = req.body; 
+     const uomId = req.params.id;
+     const { type, description, details } = req.body
  
-     if (!name || !uom || !category || !price) {
+     if (!type || !description || !details) {
          return res.status(400).json({ message: 'Missing required fields' });
      }
  
      const objectId = new ObjectId(productId); 
  
-     const updateData = { name, uom, category, price }; 
+     const updateData = { type, description, details }; 
  
      const result = await db.collection('products').updateOne(
          { _id: objectId }, 
@@ -111,14 +70,14 @@ app.put("/products/:id", async function (req, res) {
      );
  
      if (result.matchedCount === 0) {
-         res.status(404).json({ message: 'Product not found' });
+         res.status(404).json({ message: 'UoM not found' });
      } else if (result.modifiedCount === 0) {
-         res.status(200).json({ message: 'No changes made to the product' });
+         res.status(200).json({ message: 'No changes made to the UoM' });
      } else {
-         res.status(200).json({ message: 'Product updated successfully' });
+         res.status(200).json({ message: 'UoM updated successfully' });
      }
     } catch (error) {
-         res.status(500).json({ message: 'Error updating product', error: error.message });
+         res.status(500).json({ message: 'Error updating UoM', error: error.message });
     }
  });
 
